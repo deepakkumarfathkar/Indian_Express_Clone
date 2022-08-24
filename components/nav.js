5c28126de275e84e0638db5be81dc771319ae731
nileshsingh72 <rnileshchauhan89@gmail.com>
2022-08-24 20:12:35 +0530

    day-2 work done

--- components/nav.js
+++ components/nav.js
@@ -1,5 +1,5 @@
 function navbar(){
-    return `  <a href=""> <i class="fa fa-long-arrow-left" style="font-size:8px"></i> Back to Indian Express</a>
+    return `  <a href="index.html"> <i class="fa fa-long-arrow-left" style="font-size:8px"></i> Back to Indian Express</a>
     <img src="https://indianexpress.com/wp-content/themes/indianexpress/images/evolok/indian-express-logo-n.svg" alt="">`
     
 }
