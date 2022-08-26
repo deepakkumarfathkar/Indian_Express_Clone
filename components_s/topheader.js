b64fc03934618cf5b7646e8ac8fab18f3ba5b6e8
salemes58 <sbasumatas58@gmail.com>
2022-08-26 22:23:54 +0530

    fw15_411_Day-4

---
+++ components_s/topheader.js
@@ -0,0 +1,24 @@
+function topHeader(){
+    return `
+    <div id="header_s">
+        <a href="">ENGLISH</a>
+        <a href="">தமிழ்</a>
+        <a href="">বাংলা</a>
+        <a href="">മലയാളം</a>
+        <a href="">हिन्दी</a>
+        <a href="">मराठी</a>
+    </div>
+    <div class="logo_s">
+            <div class="logo_app_s">
+                <a href="https://www.facebook.com/indianexpress"><i class="fa fa-facebook-f" style="font-size:36px"></i></a>
+                <a href="https://twitter.com/indianexpress"><i class="fa fa-twitter" style="font-size:36px"></i></a>
+            </div>
+            <div class="logo_site_s">
+            <a href="./index.html"> <img src="https://indianexpress.com/wp-content/themes/indianexpress/images/indian-express-logo-n.svg" alt=""></a>
+            <p id="time_s"></p>
+
+        </div>
+    </div>
+    `
+}
+export default topHeader;
--- scripts_s/index.js
+++ scripts_s/index.js
@@ -1,20 +1,9 @@
-const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-const months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
 
-let d = new Date();
-let day = days[d.getDay()];
-let month = months[d.getMonth()];
-let year = d.getFullYear();
-const date = d.getDate();
-d.getTime();
-console.log(month, day,date,year);
-let time = document.getElementById("time_s");
-time.innerHTML = `${day}, ${month} ${date} ${year}`
 
 
 let getData = async()=>{
 
-    let res = await fetch(` https://newsapi.org/v2/top-headlines?country=in&apiKey=40e1600621784c8b9353d3c3ad8cef6b&pageSize=6`);
+    let res = await fetch(` https://newsapi.org/v2/top-headlines?country=in&apiKey=2f9406a793e0494c999bc0adce06e9c2&pageSize=6`);
     let data = await res.json();
     console.log("d",data.articles);
     appendData(data.articles);
@@ -34,12 +23,16 @@ let appendData =(data)=>{
     img.src= data[0].urlToImage;
     let p = document.createElement("h4");
     p.innerText = data[0].title;
+   
     let desc = document.createElement("p");
     desc.innerText = data[0].description;
     div.append(p,img,desc);
 
     inner_container1_s.append(div);
     data.shift()
+    div.onclick = function(){
+        window.open(data[0].url, "_blank");
+    }
     
     data.forEach(function(el){
         // console.log(el)
@@ -47,6 +40,11 @@ let appendData =(data)=>{
         let div2 = document.createElement("div");
         let title = document.createElement("p");
         title.innerText = el.title;
+        title.onclick = function(){
+            window.open(el.url, "_blank");
+        }
+        title.style.cursor = "pointer"
+        
         div2.style.borderTop = "1px dotted  gray"
         div2.style.marginTop = "10px"
         div2.style.alignItems = "center"
@@ -62,7 +60,7 @@ let appendData =(data)=>{
 let container2 = document.getElementById("container2_s");
 
 let getTopNews =async ()=>{
-    let res = await fetch(`https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=40e1600621784c8b9353d3c3ad8cef6b`);
+    let res = await fetch(`https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=2f9406a793e0494c999bc0adce06e9c2`);
     let data = await res.json();
     // console.log(data);
     appendTopNews(data.articles);
@@ -83,6 +81,10 @@ let appendTopNews =(data)=>{
     let img = document.createElement("img");
     img.src= el.urlToImage;
     div.append(title, img);
+
+    div.onclick = function(){
+        window.open(el.url, "_blank");
+    }
     top_news.append(div)
 
     })
@@ -93,7 +95,7 @@ let appendTopNews =(data)=>{
 let latest_news = document.getElementById("latest_news");
 
 let getLatestNews = async()=>{
-    let res = await fetch(`https://newsapi.org/v2/everything?q=Apple&from=2022-08-24&sortBy=popularity&apiKey=40e1600621784c8b9353d3c3ad8cef6b&pageSize=10`);
+    let res = await fetch(`https://newsapi.org/v2/everything?q=Apple&from=2022-08-24&sortBy=popularity&apiKey=2f9406a793e0494c999bc0adce06e9c2&pageSize=10`);
     let data = await res.json();
     // console.log(data.articles);
     appendLatestNews(data.articles);
@@ -113,6 +115,9 @@ let appendLatestNews =(data)=>{
     title.innerText = el.title;
    
     div.append(title);
+    div.onclick = function(){
+        window.open(el.url, "_blank");
+    }
     latest_news.append(div)
 
     })
@@ -152,8 +157,11 @@ let appendDataByCity =(data)=>{
     p.innerText = data[0].title;
     let desc = document.createElement("p");
     desc.innerText = data[0].description;
+    
     div.append(p,img,desc);
-
+    div.onclick = function(){
+        window.open(data[0].url, "_blank");
+    }
     inner1_s.append(div);
     data.shift()
     data.forEach(function(el){
@@ -162,9 +170,13 @@ let appendDataByCity =(data)=>{
         let div2 = document.createElement("div");
         let title = document.createElement("p");
         title.innerText = el.title;
+        title.onclick = function(){
+            window.open(el.url, "_blank");
+        }
         div2.style.borderTop = "1px dotted  gray"
         div2.style.marginTop = "10px"
-        div2.style.alignItems = "center"
+        div2.style.alignItems = "center";
+
         div2.append(title);
         inner2_s.append(div2);
         
@@ -175,3 +187,76 @@ let appendDataByCity =(data)=>{
 }
 
 
+
+let getExtraNews = async()=>{
+    let res = await fetch(`https://newsapi.org/v2/everything?q=technology&from=2022-08-24&sortBy=relevancy&apiKey=2f9406a793e0494c999bc0adce06e9c2&pageSize=7`);
+    let data = await res.json();
+    
+    appendExtraNews(data.articles);
+
+}
+getExtraNews();
+
+let extra_news_s = document.getElementById("extra_news_s");
+
+let appendExtraNews = (data)=>{
+    // console.log("d",data);
+    extra_news_s.innerHTML=null;
+    data.forEach(function(el){
+        let div1 = document.createElement("div");
+        let auther = document.createElement("h4");
+        auther.innerText= el.source.name
+        div1.setAttribute("class", "outer_div_s");
+        let div2 = document.createElement("div");
+        div2.setAttribute("class", "inner_div_s");
+        let title = document.createElement("p");
+        title.innerText = el.title;
+        let img = document.createElement("img");
+        img.src= el.urlToImage;
+        img.style.width = "20%"
+        div2.append(title,img)
+        div1.append(auther, div2);
+        div1.onclick = function(){
+            window.open(el.url, "_blank")
+        }
+        extra_news_s.append(div1);
+    })
+
+}
+
+let getLifestyleNews=async()=>{
+
+    let res = await fetch(`https://newsapi.org/v2/everything?q=lifestyle&from=2022-08-24&sortBy=relevancy&apiKey=2f9406a793e0494c999bc0adce06e9c2&pageSize=7`);
+    let data = await res.json();
+    console.log(data)
+    appendlifeStyleNews(data.articles);
+}
+getLifestyleNews();
+let lifestyle_s = document.getElementById("lifestyle_s");
+let appendlifeStyleNews=(data)=>{
+
+    lifestyle_s.innerHTML = null;
+    let news = document.createElement("h4");
+   
+    lifestyle_s.append(news)
+    news.innerText="LIFESTYLE";
+    news.style.fontFamily ="Arial, Helvetica, sans-serif"
+    news.style.marginBottom="5px"
+    data.forEach(function(el){
+        
+        let div= document.createElement("div");
+        div.setAttribute("class", "lifestyle_div_s");
+        let title = document.createElement("p");
+        title.innerText = el.title;
+        let img = document.createElement("img");
+        img.src= el.urlToImage;
+        img.style.width = "24%";
+        div.append(title, img);
+        div.onclick = function(){
+            window.open(el.url, "_blank")
+        }
+        lifestyle_s.append( div);
+
+    })
+
+} 
